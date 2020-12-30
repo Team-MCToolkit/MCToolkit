06dd8cc9e384ca3547a94bcb5f4e59fcf036c0cc
AlsoSomeoneElse <2000someoneelse@gmail.com>
2020-12-30 12:19:51 +0100

    Fixed some bugs with Time variable type

--- src/main/java/net/mcreator/minecraft/MinecraftImageGenerator.java
+++ src/main/java/net/mcreator/minecraft/MinecraftImageGenerator.java
@@ -890,6 +890,8 @@ public class MinecraftImageGenerator {
 					returnColor = BlocklyBlockUtil.getBlockColorFromHUE(350);
 				} else if (procedurexml.contains("<block type=\"return_blockstate\"><value name=\"return\">")) {
 					returnColor = BlocklyBlockUtil.getBlockColorFromHUE(60);
+				} else if (procedurexml.contains("<block type=\"return_time\"><value name=\"return\">")) {
+					returnColor = BlocklyBlockUtil.getBlockColorFromHUE(115);
 				}
 			}
 
