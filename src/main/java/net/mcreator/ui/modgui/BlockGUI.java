037196b52af889821f50f0198dfadaa2bf9ada5b
Max094Reikeb <maxencebekier@gmail.com>
2021-01-02 18:40:45 +0100

    Modified tests and readed world as dependency

--- src/main/java/net/mcreator/ui/modgui/BlockGUI.java
+++ src/main/java/net/mcreator/ui/modgui/BlockGUI.java
@@ -313,7 +313,7 @@ public class BlockGUI extends ModElementGUI<Block> {
 
 		redstoneCondition = new ProcedureSelector(this.withEntry("block/redstone_condition"), mcreator,
 				L10N.t("elementgui.block.event_redstone_condition"), ProcedureSelector.Side.CLIENT, true,
-				VariableElementType.NUMBER, Dependency.fromString("x:number/y:number/z:number"))
+				VariableElementType.NUMBER, Dependency.fromString("x:number/y:number/z:number/world:world"))
 				.setDefaultName(L10N.t("elementgui.common.no_additional_condition"));
 
 		generateCondition = new ProcedureSelector(this.withEntry("block/generation_condition"), mcreator,
